aa0a7f81090f79ac58973d078045e68d7a1bc2c7
Michael <trunovmichael@gmail.com>
2022-04-15 15:36:48 +0300

    Removed awk dependency.

--- greetings.sh
+++ greetings.sh
@@ -1,7 +1,9 @@
-random_selection=`od --read-bytes=1 -i /dev/urandom | head -n1 | awk '{print $2}'`
-revised_small_memes_list=("apt" "bud-frogs" "bunny" "cock" "cower" "default" "duck" "elephant" "elephant-in-snake" "eyes" "flaming-sheep" "fox" "hellokitty" "koala" "luke-koala" "moofasa" "moose" "pony-smaller" "sheep" "skeleton" "suse" "three-eyes" "tux" "unipony-smaller" "vader" "vader-koala" "www")
+random_selection=`od --read-bytes=1  -i /dev/urandom -An -tu | head -n1`
+revised_small_memes_list=( "apt" "bud-frogs" "bunny" "cock" "cower" "default" "duck" "elephant" "elephant-in-snake" "eyes" "flaming-sheep" "hellokitty" "koala" "luke-koala" "moofasa" "moose" "pony-smaller" "sheep" "skeleton" "snowman" "suse" "three-eyes" "tux" "unipony-smaller" "vader" "vader-koala" "www"  )
 
 list_size=${#revised_small_memes_list[@]}
-selection_index=$((${random_selection} % ${list_size}))
-meme_selection=${revised_small_memes_list[$selection_index]}
-ddate && ncal -3 && fortune -s | cowsay -f ${meme_selection} 
+selection=$(($random_selection % $list_size))
+meme_selection=${revised_small_memes_list[$selection]}
+ddate
+cal -3
+fortune -s | cowsay -f ${meme_selection}
